47a2c3dc74bceb4d7440e48868af063ab01fc9ba
PushkarGarg7 <97721812+PushkarGarg7@users.noreply.github.com>
2022-01-26 15:50:23 +0530

    Add files via upload

---
+++ output2.py
@@ -0,0 +1,51 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:/Users/lionp/Downloads/assignment2.py ===============
+The length of string is 35
+The reversed string is  egaugnal evitisnes esac a si nohtyP
+a case sensitive
+object oriented
+The index of substring a is  10
+The origial string after removing white spaces is Pythonisacasesensitivelanguage
+
+
+Enter your name
+=============== RESTART: C:/Users/lionp/Downloads/assignment2.py ===============
+The length of string is 35
+The reversed string is  egaugnal evitisnes esac a si nohtyP
+Python is object oriented language
+The index of substring a is  10
+The origial string after removing white spaces is Pythonisacasesensitivelanguage
+
+
+Enter your name Pushkar Garg
+Enter your SID 21103038
+Enter your department CSE
+Enter your CGPA 10
+Hey  Pushkar Garg, Here!
+My SID is 21103038
+I am from  CSE and my CGPA is 10.000000
+
+
+a.  8
+b.  58
+c.  50
+Left shift both a and b with 2 bits respectively : 224 40
+Right shift a with 2 bits and b with 4 bits respectively :  14 0
+
+
+Enter first number: 231
+Enter second number: 214
+Enter third number: 197
+THE LARGEST NUMBER IS  231.0
+
+
+Enter a string my name is pushkar
+Yes
+
+
+Enter first side of the triangle 41
+Enter second side of the triangle40
+Enter third side of the triangle9
+Yes
