2c2ca986215f37f09f7e563ef5956be7e4579d50
PushkarGarg7 <97721812+PushkarGarg7@users.noreply.github.com>
2022-02-14 23:55:23 +0530

    Add files via upload

---
+++ output3.py
@@ -0,0 +1,87 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:/Users/lionp/Downloads/assignment3.py ===============
+Enter a string-PushkarGarg
+{'P': 1, 'U': 1, 'S': 1, 'H': 1, 'K': 1, 'A': 2, 'R': 2, 'G': 2}
+Enter month-10
+Give day:4
+Give year:2002
+Date: 4 / 10 / 2002
+Next Date: 5 / 10 / 2002
+[(1, 1), (2, 4), (3, 9), (4, 16), (5, 25), (6, 36), (7, 49), (8, 64), (9, 81), (10, 100)]
+Enter a number from 4 to 10:7
+Performance=Good
+Letter Grade=B
+Traceback (most recent call last):
+  File "C:/Users/lionp/Downloads/assignment3.py", line 134, in <module>
+    print(" "*(variable-1),Word[0:11-((variable-1)*2)])
+NameError: name 'Word' is not defined. Did you mean: 'ord'?
+
+=============== RESTART: C:/Users/lionp/Downloads/assignment3.py ===============
+Enter a string-PushkarGarg
+{'P': 1, 'U': 1, 'S': 1, 'H': 1, 'K': 1, 'A': 2, 'R': 2, 'G': 2}
+Enter month-10
+Give day:4
+Give year:2002
+Date: 4 / 10 / 2002
+Next Date: 5 / 10 / 2002
+[(1, 1), (2, 4), (3, 9), (4, 16), (5, 25), (6, 36), (7, 49), (8, 64), (9, 81), (10, 100)]
+Enter a number from 4 to 10:7
+Performance=Good
+Letter Grade=B
+ ABCDEFGHIJK
+  ABCDEFGHI
+   ABCDEFG
+    ABCDE
+     ABC
+      A
+(a):
+enter sid:21103038
+Enter your name:PushkarGarg
+give a letter Y or N:Y
+The required dictionary: {21103038: 'PushkarGarg'}
+The list will be used in further parts:
+The required list: [(21103038, 'PushkarGarg')]
+(b):
+The dictionary we had before:
+{21103038: 'PushkarGarg'}
+exchanged the key value pair but its not sorted:
+{'PushkarGarg': 21103038}
+unsorted list:
+[('PushkarGarg', 21103038)]
+sorted list:
+[('PushkarGarg', 21103038)]
+sorted dictionary:
+{'PushkarGarg': 21103038}
+This is the dictionary sorted on the basis of name-
+{21103038: 'PushkarGarg'}
+(c):
+The earlier list will be used now:
+[(21103038, 'PushkarGarg')]
+now the list is sorted-
+[(21103038, 'PushkarGarg')]
+sorted dictionary based on sid: {21103038: 'PushkarGarg'}
+(d):
+enter one of the previous sid's entered:21103038
+The name of the student with the entered sid is:
+PushkarGarg
+enter a number:7
+enter another a number:17
+[7, 17, 24]
+Give y to contnue and n to stop:n
+we have a list having a fibonacci series:
+[7, 17, 24]
+Average of the sequence:
+16.0
+(a):
+{1, 3, 5, 6, 8}
+(b):
+{3, 6, 8, 9, 13, 17}
+(c):
+{1, 2, 4, 5}
+(d)
+{6, 7, 8, 9, 10}
+-------------(e)------------
+{10, 7}
+Done!
