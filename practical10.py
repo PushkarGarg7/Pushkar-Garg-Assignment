546c224a4f1fb452c049c6856a2dc8af8a532dd4
PushkarGarg7 <97721812+PushkarGarg7@users.noreply.github.com>
2022-02-24 17:04:16 +0530

    Add files via upload

---
+++ practical10.py
@@ -0,0 +1,15 @@
+#factorial
+
+n=int(input("enter a postive integer:"))
+for i in range(1,n):
+    n=n*i
+print(n)
+
+#sum of natural number
+
+n=int(input("enter limit:"))
+sum=0
+for i in range(1,n+1):
+    sum=sum+i
+print(sum)
+
